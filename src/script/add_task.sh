37d85cf79bed2013a2cd51d0dadb0c517664574b
bassyy <bassy8425@gmail.com>
2024-06-13 08:48:17 +0900

    改行を入れた

--- src/script/add_task.sh
+++ src/script/add_task.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # タスク名、期限、メモを取得するフォームの表示
-form_result=$(zenity --forms --title="新しいタスク" --text="タスクの詳細を入力してください。" \
+form_result=$(
+    zenity --forms \
+    --title="新しいタスク" \
+    --text="タスクの詳細を入力してください。" \
     --add-entry="タスク名" \
     --add-calendar="期限" \
-    --add-entry="メモ")
+    --add-entry="メモ"
+    )
 
-# Zenityフォームの結果を標準出力に表示
+# 入力結果をPythonスクリプトに渡す
 echo "$form_result"
